46b600495862049ef9dbb96327d8b692dce301e6
kanapo <kanapodream@gmail.com>
2021-01-15 15:08:50 +0900

    fix

--- run.py
+++ run.py
@@ -3,10 +3,10 @@ import sys
 
 
 def run():
-    name = "sample/bubblesort.grim"
+    name = "grim/test/sample/bubblesort.grim"
     # name = "grim/test/testprogram.grim"
     DebugAndRun.run(file=open(name, encoding="utf-8"),
-                    debug=True, running=True, logfile=open("debug_msg.txt", mode="w",encoding="utf-8"))
+                    debug=False, running=True, logfile=open("debug_msg.txt", mode="w",encoding="utf-8"))
 
 
 if __name__ == "__main__":
